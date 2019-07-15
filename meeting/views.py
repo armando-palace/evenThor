07342f5f158820132b1784a23b093e00ecea10e8
Jesús Di Bari <iijbari@gmail.com>
2019-07-15 13:31:15 -0400

    last changes

--- meeting/views.py
+++ meeting/views.py
@@ -1,8 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from meeting.models import Event, Presentation, Speaker, Assistant
 
 # Create your views here.
 def index(request):
-  my_dictionary = {'insert_me': 'Hello!'}
-  return render(request, 'meeting/index.html', context=my_dictionary)
+  events = Event.objects.all()
+  context = { 'events': events }
+  return render(request, 'meeting/index.html', context)
+
+def event_show(request, event_id):
+  event = get_object_or_404(Event, pk=event_id)
+  return render(request, 'meeting/show_event.html', { 'event': event })
